418e2d788479d442a918114f2da958f541ca92de
kabaskimy <kabaskimy@gmail.com>
2013-03-05 18:08:17 +0800

    T4Example

---
+++ T4Exmaple.ConsoleExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace T4Exmaple.ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo currentDir = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine();
+            string combineDir = Path.Combine(currentDir.Parent.Parent.FullName, "ResourceCSV");
+            //Console.WriteLine(combineDir);
+            if (Directory.Exists(combineDir))
+            {
+                DirectoryInfo resourceDir = new DirectoryInfo(combineDir);
+                FileInfo[] files = resourceDir.GetFiles("*.xml", SearchOption.AllDirectories);
+                foreach (FileInfo item in files)
+                {
+                    Console.WriteLine(item.Directory.Parent.Name);
+                    Console.WriteLine(item.Name.Replace(".xml",string.Empty));
+                    Console.WriteLine(item.FullName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Folder doesn't exist");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
