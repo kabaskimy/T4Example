321e654ef67622dec6dbad52710f84ba894d2c79
kabaskimy <kabaskimy@gmail.com>
2013-03-06 18:17:20 +0800

    更新

---
+++ T4Exmaple.ConsoleExample/CsvDataProvider.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+using System.Data;
+
+namespace T4Exmaple.ConsoleExample
+{
+    public class CsvDataProvider
+    {
+        public CsvDataProvider()
+        {
+        }
+
+        public CsvDataProvider(string folderPath)
+        {
+            FolderPath = folderPath;
+            FileNameList = new List<FileInfo>();
+            AllDataTable = new DataSet();
+            if (Directory.Exists(folderPath))
+            {
+
+                DirectoryInfo resourceDir = new DirectoryInfo(Path.Combine(folderPath,"ResourceCSV"));
+                //Console.WriteLine(resourceDir.FullName);
+                FileInfo[] files = resourceDir.GetFiles("*.xls?", SearchOption.AllDirectories);
+                foreach (FileInfo item in files)
+                {
+                    FileNameList.Add(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Folder doesn't exist");
+            }
+
+            foreach (FileInfo item in FileNameList)
+            {
+                InitializeWorkbook(item);
+                ConvertToDataTable(item);
+            }
+        }
+
+        public string FolderPath
+        {
+            set;
+            get;
+        }
+
+        public DataSet AllDataTable
+        {
+            private set;
+            get;
+        }
+
+        public List<FileInfo> FileNameList
+        {
+            private set;
+            get;
+        }
+        
+        HSSFWorkbook hssfworkbook;
+        private void InitializeWorkbook(FileInfo fileName)
+        {
+            //read the template via FileStream, it is suggested to use FileAccess.Read to prevent file lock.
+            //book1.xls is an Excel-2007-generated file, so some new unknown BIFF records are added. 
+            using (FileStream file = new FileStream(fileName.FullName, FileMode.Open, FileAccess.Read))
+            {
+                hssfworkbook = new HSSFWorkbook(file);
+            }
+        }
+
+        private void ConvertToDataTable(FileInfo item)
+        {
+            ISheet sheet = hssfworkbook.GetSheetAt(0);
+            System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
+            
+
+            string tableFileName=item.Name.Substring(0,item.Name.Length-item.Extension.Length);
+            //for (int j = 0; j < 5; j++)
+            //{
+            //    dt.Columns.Add(Convert.ToChar(((int)'A') + j).ToString());
+            //}
+            int columnNumber = 0;
+            List<DataTable> multiTables = new List<DataTable>();
+            if (rows.MoveNext())
+            {
+                IRow collumnRow = (HSSFRow)rows.Current;
+                for (int i = 0; i < collumnRow.LastCellNum; i++)
+                {
+                    ICell collumnCell = collumnRow.GetCell(i);
+                    if (collumnCell != null && !String.IsNullOrEmpty(collumnCell.ToString().Trim()))
+                    {
+                        //dt.Columns.Add(collumnCell.ToString().Trim());
+                        if (i!= 0)
+                        {
+                            DataTable multiData = new DataTable(tableFileName + "." + collumnCell.ToString().Trim());
+                            multiData.Columns.Add("key");
+                            multiData.Columns.Add(collumnCell.ToString().Trim());
+                            multiTables.Add(multiData);
+                        }
+                        columnNumber++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            
+            while (rows.MoveNext())
+            {
+                IRow row = (HSSFRow)rows.Current;
+                //DataRow dr = dt.NewRow();
+
+                string key=string.Empty;
+                for (int i = 0; i < row.LastCellNum && i < columnNumber; i++)
+                {
+                    ICell cell = row.GetCell(i);
+                    if (i == 0)
+                    {
+                        key = cell.ToString().Trim();
+                    }
+                    else
+                    {
+                        DataRow dr = multiTables[i - 1].NewRow();
+                        dr[0] = key;
+                        dr[1] = cell.ToString().Trim();
+                        multiTables[i - 1].Rows.Add(dr);
+                    }
+
+
+                }
+
+            }
+            foreach (DataTable dt in multiTables)
+            {
+                AllDataTable.Tables.Add(dt);
+            }
+        }
+
+    }
+}
--- T4Exmaple.ConsoleExample/Program.cs
+++ T4Exmaple.ConsoleExample/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
 
 namespace T4Exmaple.ConsoleExample
 {
@@ -11,26 +12,33 @@ namespace T4Exmaple.ConsoleExample
     {
         static void Main(string[] args)
         {
-            DirectoryInfo currentDir = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory);
-            Console.WriteLine();
-            string combineDir = Path.Combine(currentDir.Parent.Parent.FullName, "ResourceCSV");
-            //Console.WriteLine(combineDir);
-            if (Directory.Exists(combineDir))
+            //DirectoryInfo currentDir = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory);
+            //Console.WriteLine();
+            //string combineDir = Path.Combine(currentDir.Parent.Parent.FullName, "ResourceCSV");
+            ////Console.WriteLine(combineDir);
+            //if (Directory.Exists(combineDir))
+            //{
+            //    DirectoryInfo resourceDir = new DirectoryInfo(combineDir);
+            //    FileInfo[] files = resourceDir.GetFiles("*.xml", SearchOption.AllDirectories);
+            //    foreach (FileInfo item in files)
+            //    {
+            //        Console.WriteLine(item.Directory.Parent.Name);
+            //        Console.WriteLine(item.Name.Replace(".xml",string.Empty));
+            //        Console.WriteLine(item.FullName);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Folder doesn't exist");
+            //}
+
+            CsvDataProvider provider = new CsvDataProvider(new DirectoryInfo(@"..\").Parent.FullName);
+            Console.WriteLine(provider.AllDataTable.Tables.Count);
+            //DataTable table = provider.AllDataTable.Tables[1];
+            foreach (DataTable item in provider.AllDataTable.Tables)
             {
-                DirectoryInfo resourceDir = new DirectoryInfo(combineDir);
-                FileInfo[] files = resourceDir.GetFiles("*.xml", SearchOption.AllDirectories);
-                foreach (FileInfo item in files)
-                {
-                    Console.WriteLine(item.Directory.Parent.Name);
-                    Console.WriteLine(item.Name.Replace(".xml",string.Empty));
-                    Console.WriteLine(item.FullName);
-                }
+                Console.WriteLine(item.TableName);
             }
-            else
-            {
-                Console.WriteLine("Folder doesn't exist");
-            }
-            
             Console.ReadLine();
         }
     }
